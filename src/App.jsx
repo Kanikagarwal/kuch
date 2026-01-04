2c1b4b4a824ce2457a959de9c737dff4d0d42715
Kanika Agarwal <kanikaweb26@gmail.com>
2026-01-04 22:08:39 +0530

    Clean project without large files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './Pages/Landing'
 import All from './Pages/All.jsx'
